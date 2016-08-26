0bcd32aa3d7db3f1bf398a2db4c7105f53606c7c
Harmitage <regharmitage@gmail.com>
2016-08-26 18:27:05 +0500

    Added JMonkey support TODO: remove RabbitMQ and/or Spring

---
+++ src/main/kotlin/com/gsg/Receiver.kt
@@ -0,0 +1,14 @@
+package com.gsg
+
+import java.util.concurrent.CountDownLatch
+
+class Receiver {
+
+    val latch = CountDownLatch(1)
+
+    fun receiveMessage(message: String) {
+        println("Received <$message>")
+        latch.countDown()
+    }
+
+}
